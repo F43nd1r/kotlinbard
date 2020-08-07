5eb49cbc1a299539db55f6aef92a03c9ff8f12e6
Benjamin Ye <BobBenBan@users.noreply.github.com>
2020-08-07 11:39:00 -0700

    Create SpecDelegates manual codegen

---
+++ codegen/src/manual/kotlin/SpecDelegates.kt
@@ -0,0 +1,130 @@
+/*
+ *    Copyright 2020 Benjamin Ye
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package io.github.enjoydambience.kotlinbard.codegen
+
+import com.squareup.kotlinpoet.FileSpec
+import com.squareup.kotlinpoet.KModifier
+import com.squareup.kotlinpoet.asTypeName
+import com.squareup.kotlinpoet.asTypeVariableName
+import io.github.enjoydambience.kotlinbard.*
+import io.github.enjoydambience.kotlinbard.codegen.generators.FileGenerator
+import io.kotest.core.spec.style.StringSpec
+import java.io.OutputStreamWriter
+import java.nio.charset.StandardCharsets
+import java.nio.file.Files
+import java.nio.file.Path
+import kotlin.reflect.*
+import kotlin.reflect.full.declaredMembers
+
+/**
+ * Generates classes which delegate everything to a Spec.
+ *
+ * The output file currently is not entirely semantically correct, and needs manual editing.
+ */
+object SpecDelegates : FileGenerator {
+    private const val delegateName = "poetType"
+    override fun FileSpec.Builder.generate() {
+        SpecInfo.allSpecs.forEach {
+            addDelegateClass(it.specClass, it.name + "Delegate")
+            addDelegateClass(it.builderClass, it.name + "BuilderDelegate")
+        }
+    }
+
+    private fun FileSpecBuilder.addDelegateClass(klass: KClass<*>, name: String) = addClass(name) {
+        primaryConstructor {
+            addParameter(delegateName, klass, KModifier.INTERNAL)
+        }
+        addProperty(delegateName, klass) { init { add(delegateName) } }
+
+        klass.supertypes.forEach { t ->
+            if ((t.classifier as? KClass<*>)?.java?.isInterface == true) {
+                addSuperinterface(t.asTypeName(), delegateName)
+            }
+        }
+
+        val superMembers = klass.supertypes.flatMap {
+            (it.classifier as KClass<*>).declaredMembers
+        }
+
+        klass.declaredMembers.forEach {
+            if (it.visibility != KVisibility.PUBLIC) return@forEach
+            when (it) {
+                is KProperty<*> -> delProperty(it, superMembers)
+                is KFunction<*> -> delFunction(it, superMembers)
+                else -> error("member not a function or property")
+            }
+        }
+    }
+
+    private fun TypeSpecBuilder.delProperty(prop: KProperty<*>, superMembers: List<KCallable<*>>) {
+        if (superMembers.any { it is KProperty && it.signature == prop.signature }) return
+        addProperty(prop.name, prop.returnType.asTypeName()) {
+            get {
+                addStatement("return $delegateName.%N", prop.name)
+            }
+            if (prop is KMutableProperty<*>) {
+                mutable()
+                set("value") {
+                    addStatement("$delegateName.%N = value", prop.name)
+                }
+            }
+        }
+    }
+
+    private fun TypeSpecBuilder.delFunction(func: KFunction<*>, superMembers: List<KCallable<*>>) {
+        if (superMembers.any { it is KFunction && it.signature == func.signature }) return
+        addFunction(func.name) {
+            addModifiers(KModifier.PUBLIC)
+            returns(func.returnType.asTypeName())
+            addTypeVariables(func.typeParameters.map { it.asTypeVariableName() })
+
+            val (call, params) = reflectCodeCall(func, delegateName)
+            addParameters(params)
+            addStatement("return $call")
+        }
+    }
+}
+
+private val kFunctionSigField = Class.forName("kotlin.reflect.jvm.internal.KFunctionImpl")
+    .getDeclaredField("signature").apply { isAccessible = true }
+private val KFunction<*>.signature: String?
+    get() = kFunctionSigField[this] as String?
+private val kPropertySigField = Class.forName("kotlin.reflect.jvm.internal.KPropertyImpl")
+    .getDeclaredField("signature").apply { isAccessible = true }
+private val KProperty<*>.signature: String?
+    get() = kPropertySigField[this] as String?
+
+class GenerateDelegates : StringSpec({
+    val destination = "./build/generated-src"
+    val path = Path.of(destination).toAbsolutePath()
+    "generateDelegates" {
+        println("generating to $path")
+        buildString {
+            SpecDelegates.createFileSpec().writeTo(this)
+        }.replace("public get\\(\\)".toRegex(), "get()")
+            .replace("\n *get".toRegex(), " get")
+            .replace("((get\\(\\)|fun).*\n)\n".toRegex(), "$1")
+            .replace("\n\n".toRegex(), "\n")
+            .let {
+                val outputPath = path.resolve("${SpecDelegates.fileName}.kt")
+                OutputStreamWriter(
+                    Files.newOutputStream(outputPath),
+                    StandardCharsets.UTF_8
+                ).use { writer -> writer.append(it) }
+            }
+    }
+})
